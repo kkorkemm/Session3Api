ec95c9732a0b06fd1d871ce9ffef8e0c0215e1b4
korkem <73012254+kkorkemm@users.noreply.github.com>
2021-10-19 13:44:03 +0600

    nya

---
+++ Session3Api/Controllers/AssetOdometersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Session3Api.Base;
+
+namespace Session3Api.Controllers
+{
+    public class AssetOdometersController : ApiController
+    {
+        private KazanNeftSession3DBEntities db = new KazanNeftSession3DBEntities();
+
+        // GET: api/AssetOdometers
+        [ResponseType(typeof(List<AssetOdometers>))]
+        public IHttpActionResult GetAssetOdometers()
+        {
+            return Ok(db.AssetOdometers);
+        }
+
+        // GET: api/AssetOdometers/5
+        [ResponseType(typeof(AssetOdometers))]
+        public IHttpActionResult GetAssetOdometers(long id)
+        {
+            AssetOdometers assetOdometers = db.AssetOdometers.Find(id);
+            if (assetOdometers == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(assetOdometers);
+        }
+    }
+}
